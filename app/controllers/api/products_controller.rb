5dbb710ba173b621e3efb8eb1ae3a168a5d321b8
Mishy <michelleelhai@gmail.com>
2019-08-28 11:02:45 -0700

    adds product route to display all

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,6 @@
 class Api::ProductsController < ApplicationController
+  def allproducts
+    @all = Product.all
+    render 'allproducts.json.jb'
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+product = Product.create({name:"heated blanket",
+ price: 45.99, 
+ image_url:"https://s.yimg.com/aah/yhst-11545572856523/serta-designer-medallion-quilted-heated-blanket-king-36.png", 
+ description: "a fluffy goodness that you wrap yourself in like a burrito."})
+product = Product.create({name:"A badass sidekick", 
+  price: 129.99, 
+  image_url:"https://vignette.wikia.nocookie.net/disney/images/6/69/Profile_-_Iago.jpeg/revision/latest?cb=20190312023536", 
+  description: "Your hypemna, your bestie, and your protector all in one."})
+product = Product.create({name:"coffee",
+ price: 1.29, 
+ image_url: "https://media.timeout.com/images/105280507/1372/772/image.jpg", 
+ description: "A hot steaming cuo fo love"})
